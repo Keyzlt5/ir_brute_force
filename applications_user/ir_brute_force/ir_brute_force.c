0fe7cf696155234ac4d3b2728d630c17aff68ba3
Keyzlt5 <144552102+Keyzlt5@users.noreply.github.com>
2025-07-16 16:22:04 -0400

    Update ir_brute_force.c

--- applications_user/ir_brute_force/ir_brute_force.c
+++ applications_user/ir_brute_force/ir_brute_force.c
@@ -1,41 +1,64 @@
-// ir_brute_force.c
 #include <furi.h>
 #include <furi_hal.h>
 #include <gui/gui.h>
-#include <input/input.h>
+#include <gui/view_dispatcher.h>
+#include <gui/view.h>
+#include <gui/modules/submenu.h>
+#include <gui/modules/dialog_ex.h>
+#include <notification/notification_messages.h>
 #include <infrared/infrared.h>
-#include <stdlib.h>
+
+#define APP_IR_BRUTE_FORCE "IR Brute Force"
+
+typedef struct {
+    ViewDispatcher* view_dispatcher;
+    Submenu* submenu;
+    Infrared* infrared;
+} IrBruteForceApp;
+
+static void ir_brute_force_send_dummy_signal(IrBruteForceApp* app) {
+    InfraredMessage msg = {
+        .protocol = InfraredProtocolUnknown,
+        .address = 0x00,
+        .command = 0x01,
+        .repeat = 1
+    };
+    infrared_send_command(app->infrared, &msg);
+}
+
+static void ir_brute_force_menu_callback(void* context, uint32_t index) {
+    IrBruteForceApp* app = context;
+    switch(index) {
+        case 0:
+            ir_brute_force_send_dummy_signal(app);
+            break;
+    }
+}
+
+static void ir_brute_force_app_free(IrBruteForceApp* app) {
+    view_dispatcher_remove_view(app->view_dispatcher, 0);
+    submenu_free(app->submenu);
+    view_dispatcher_free(app->view_dispatcher);
+    infrared_free(app->infrared);
+    furi_record_close(RECORD_IR);
+    free(app);
+}
 
 int32_t ir_brute_force_app(void* p) {
     UNUSED(p);
-    FuriHalPowerProfile profile = furi_hal_power_get_profile();
-    furi_hal_power_enable_otg();
+    IrBruteForceApp* app = malloc(sizeof(IrBruteForceApp));
 
-    Infrared* infrared = infrared_alloc();
-    if(!infrared) return -1;
+    app->infrared = furi_record_open(RECORD_IR);
+    app->view_dispatcher = view_dispatcher_alloc();
+    app->submenu = submenu_alloc();
 
-    const uint32_t delay_ms = 250;
+    submenu_add_item(app->submenu, "Send Dummy IR Signal", 0, ir_brute_force_menu_callback, app);
 
-    for(uint32_t code = 0; code <= 0xFFFF; ++code) {
-        uint8_t data[2];
-        data[0] = (code >> 8) & 0xFF;
-        data[1] = code & 0xFF;
-        
-        InfraredRawFrame* frame = infrared_raw_frame_alloc();
-        frame->protocol = InfraredProtocolNEC;
-        frame->address = data[0];
-        frame->command = data[1];
-        frame->repeat = false;
+    view_dispatcher_add_view(app->view_dispatcher, 0, submenu_get_view(app->submenu));
+    view_dispatcher_switch_to_view(app->view_dispatcher, 0);
 
-        infrared_transmit_raw_frame(infrared, frame);
-        infrared_raw_frame_free(frame);
+    view_dispatcher_run(app->view_dispatcher);
 
-        furi_delay_ms(delay_ms);
-    }
-
-    infrared_free(infrared);
-    furi_hal_power_disable_otg();
-    furi_hal_power_set_profile(profile);
-
+    ir_brute_force_app_free(app);
     return 0;
 }
