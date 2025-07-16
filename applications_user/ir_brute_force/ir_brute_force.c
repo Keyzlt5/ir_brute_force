d8a970bbd44d927da5911e7846e500e6af2b658b
keyzlt5 <kyleschiele92@gmail.com>
2025-07-16 09:41:07 -0400

    Full reset and upload of Flipper IR Brute Force app

---
+++ applications_user/ir_brute_force/ir_brute_force.c
@@ -0,0 +1,8 @@
+#include <furi.h>
+#include <gui/gui.h>
+
+int32_t ir_brute_force_app(void* p) {
+    UNUSED(p);
+    printf("IR Brute Force running...\n");
+    return 0;
+}
---
+++ flipperzero-firmware/applications_user/ir_brute_force/ir_brute_force.c
@@ -0,0 +1,1 @@
+// IR Brute Force app source code
